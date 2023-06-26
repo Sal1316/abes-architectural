bbedb7d4fc51208fc4398ff106809163e27620d0
Sal Ceja <salceja45@gmail.com>
2023-06-26 10:11:39 -0700

    deteted json files ourside of main

---
+++ abes-architec/src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <ul className="horizontle-list">
+          <li>Home</li>
+          <li>Services</li>
+          <li>Gallery</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
